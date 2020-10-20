b8d5a813f417b361e7e92094a0ceaefb1444eaf5
Aya AbdulSlam Hemaid <aya.abdel.slam93@gmail.com>
2020-10-20 23:49:11 +0200

    Processing large collections

--- StockAnalyzer.Windows/MainWindow.xaml.cs
+++ StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -68,54 +68,67 @@ namespace StockAnalyzer.Windows
                 }
                 #endregion
 
-                var loadedStocks = (await Task.WhenAll(tickerLoadingTasks)).SelectMany(stock => stock);
+                var loadedStocks = await Task.WhenAll(tickerLoadingTasks);
 
                 // it will block out the ui
                 // it will never begin nor complete in order
                 // same for Paraller.Foreach
                 // Parallel.For
                 // Blocks UI Thread
-                Parallel.Invoke(() =>
+                #region Intro Paraller Extention
+                //Parallel.Invoke(() =>
+                //{
+                //    Debug.WriteLine("Starting operation 1");
+
+                //    CalculateExpensiveComputation(loadedStocks);
+
+                //    Debug.WriteLine("Completed operation 1");
+
+                //    // if we use dispatcher like the Task lib it will DEADLOCK
+                //    //Dispatcher.Invoke(() =>
+                //    //{
+
+                //    //});
+
+                //},
+                //()=>{
+                //    Debug.WriteLine("Starting operation 2");
+
+                //    CalculateExpensiveComputation(loadedStocks);
+
+                //    Debug.WriteLine("Completed operation 2");
+
+                //},
+                //()=>{
+                //    Debug.WriteLine("Starting operation 3");
+
+                //    CalculateExpensiveComputation(loadedStocks);
+
+                //    Debug.WriteLine("Completed operation 3");
+
+                //},
+                //()=>{
+                //    Debug.WriteLine("Starting operation 4");
+
+                //    CalculateExpensiveComputation(loadedStocks);
+
+                //    Debug.WriteLine("Completed operation 4");
+
+                //});
+                #endregion
+
+                var values = new ConcurrentBag<StockCalculation>();
+                Parallel.ForEach(loadedStocks, (stocks) =>
                 {
-                    Debug.WriteLine("Starting operation 1");
+                    var data = new StockCalculation
+                    {
+                        Result = CalculateExpensiveComputation(stocks),
+                        Ticker = stocks.First().Ticker
+                    };
 
-                    CalculateExpensiveComputation(loadedStocks);
-
-                    Debug.WriteLine("Completed operation 1");
-
-                    // if we use dispatcher like the Task lib it will DEADLOCK
-                    //Dispatcher.Invoke(() =>
-                    //{
-
-                    //});
-
-                },
-                ()=>{
-                    Debug.WriteLine("Starting operation 2");
-
-                    CalculateExpensiveComputation(loadedStocks);
-
-                    Debug.WriteLine("Completed operation 2");
-
-                },
-                ()=>{
-                    Debug.WriteLine("Starting operation 3");
-
-                    CalculateExpensiveComputation(loadedStocks);
-
-                    Debug.WriteLine("Completed operation 3");
-
-                },
-                ()=>{
-                    Debug.WriteLine("Starting operation 4");
-
-                    CalculateExpensiveComputation(loadedStocks);
-
-                    Debug.WriteLine("Completed operation 4");
-
+                    values.Add(data);
                 });
-                
-                Stocks.ItemsSource = loadedStocks;
+                Stocks.ItemsSource = values.ToArray();
             }
             catch (Exception ex)
             {
